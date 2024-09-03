1ecd8cdbae1b0b7e237b5348f5ba574e479ce481
Amer <a.abdalrazak@transtek.com>
2024-09-03 13:31:13 +0300

    Add project files.

---
+++ LargestRectangleConsoleApp/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace LargestRectangleConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const int numRectangles = 10;
+            Random random = new Random();
+            Rectangle[] rectangles = new Rectangle[numRectangles];
+
+            // Generate random rectangles
+            for (int i = 0; i < numRectangles; i++)
+            {
+                rectangles[i] = new Rectangle
+                {
+                    Width = random.Next(1, 101),  // Width between 1 and 100
+                    Height = random.Next(1, 101)  // Height between 1 and 100
+                };
+
+                Console.WriteLine(rectangles[i]);
+            }
+
+            // Find the largest rectangle
+            Rectangle largestRectangle = FindLargestRectangle(rectangles);
+            Console.WriteLine($"\nLargest Rectangle: {largestRectangle}");
+        }
+
+        static Rectangle FindLargestRectangle(Rectangle[] rectangles)
+        {
+            Rectangle largest = rectangles[0];
+
+            foreach (var rectangle in rectangles)
+            {
+                if (rectangle.Area > largest.Area ||
+                    (rectangle.Area == largest.Area && rectangle.Height > largest.Height))
+                {
+                    largest = rectangle;
+                }
+            }
+
+            return largest;
+        }
+    }
+}
---
+++ LargestRectangleConsoleApp/Rectangle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LargestRectangleConsoleApp
+{
+    class Rectangle
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+
+        public int Area
+        {
+            get
+            {
+                return Width * Height;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"Rectangle [Width: {Width}, Height: {Height}, Area: {Area}]";
+        }
+    }
+}
